2f261c92650404ff89f61d643dbe7515eb7c1d8c
Advaita <148793147+01adv@users.noreply.github.com>
2024-01-10 05:38:14 +0000

    ROUTING-TUT

--- redirecting-routing/src/App.jsx
+++ redirecting-routing/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
-      <h1>
-        React
-      </h1>
+      <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
