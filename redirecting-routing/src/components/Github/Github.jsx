bc9fbbe379b0ebcdb16395f6ece560d96b6c306a
Advaita <148793147+01adv@users.noreply.github.com>
2024-01-12 15:05:32 +0000

    loader functionality added

--- redirecting-routing/src/components/Github/Github.jsx
+++ redirecting-routing/src/components/Github/Github.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Github = () => {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
 
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetch('https://api.github.com/users/01adv').then(response => response.json()).then(data => setData(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/01adv').then(response => response.json()).then(data => setData(data))
+    // }, [])
     return (
         <div className="text-center m-4 bg-gray-500 text-white p-4 text-3xl">Github Followers: {data.followers}
             <img src={data.avatar_url} alt="Git Picture" width={300}
@@ -15,3 +16,11 @@ const Github = () => {
 }
 
 export default Github
+
+
+
+//this info loader is used in main file, where it is called in github route, then data is passed to the return objects like image and followers using useloaderData function 
+export const githubInfoLoader = async() =>{
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
